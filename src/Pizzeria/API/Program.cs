1252864ff51d9f9762067146b24776419bc640c6
Misael <misaelpiucaet12d1@gmail.com>
2026-09-06 21:26:13 -0300

    Eliminar referencias a Background y dejar constancia: quitar cola/HostedService (simplificación académica)

--- src/Pizzeria/API/Program.cs
+++ src/Pizzeria/API/Program.cs
@@ -4,7 +4,6 @@ using Pizzeria.Dominio.Interfaces;
 using Pizzeria.Persistencia.Repos;
 using Pizzeria.Servicios.Interface;
 using Pizzeria.Servicios.Service;
-using Pizzeria.Servicios.Background;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,10 +21,6 @@ builder.Services.AddScoped<IPizzaRepository, PizzaRepository>();
 builder.Services.AddScoped<IPedidoService, PedidoService>();
 builder.Services.AddScoped<IClienteService, ClienteService>();
 builder.Services.AddScoped<IPizzaService, PizzaService>();
-
-// Background queue and simulator
-builder.Services.AddSingleton<IPedidoProcessingQueue, PedidoProcessingQueue>();
-builder.Services.AddHostedService<PedidoStateSimulator>();
 
 var app = builder.Build();
 
---
+++ src/Pizzeria/Servicios/Background/README_REMOVIDO.txt
@@ -0,0 +1,4 @@
+// La carpeta Servicios/Background ha sido eliminada de este branch porque la simulación
+// de estados se implementa de forma ligera y encapsulada dentro de PedidoService
+// (Task.Run con scope). No existe uso ni dependencia de BackgroundService en el código
+// actual del branch MVC.
