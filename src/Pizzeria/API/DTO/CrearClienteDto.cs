aa0fe26329a2996f7cb203fb43e1481e796c386e
Misael <misaelpiucaet12d1@gmail.com>
2026-09-06 21:13:00 -0300

    Add CrearClienteDto for request validation

---
+++ src/Pizzeria/API/DTO/CrearClienteDto.cs
@@ -0,0 +1,19 @@
+using Pizzeria.API.DTO;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pizzeria.API.DTO;
+
+public class CrearClienteDto
+{
+    [Required]
+    [StringLength(100, MinimumLength = 2)]
+    public string Nombre { get; set; } = string.Empty;
+
+    [Required]
+    [Phone]
+    public string Telefono { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(200)]
+    public string Direccion { get; set; } = string.Empty;
+}
